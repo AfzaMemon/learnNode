e7bcbb8bb0eddbbb74072b08d4f0b1a82640aeba
AfzaMemon <afzamemon2002@gmail.com>
2025-06-05 23:42:50 +0530

    Added math file and learn Modules

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
+const math =require('./maths');
 console.log("hello world");
+console.log("add:  ",math.add(2,4));
+console.log("sub:  ",math.sub(2,4));
+// console.log("objAdd: ",math.objAdd(2,3));
+// console.log("objSub:  ",math.objSub(2,3));
---
+++ maths.js
@@ -0,0 +1,12 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+return a-b;
+}
+// exports.objAdd=(a,b)=>{
+//    return a+b;
+
+// }
+// exports.objSub=(a,b)=>{return-b}
+module.exports= {add,sub};
